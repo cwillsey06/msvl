c3a2ba14ea0dc48fad9d42e5d99c806e6d403002
Coltrane Willsey <cwillsey06@gmail.com>
2024-04-16 11:02:27 -0600

    Fix stupid column indentation

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 --2023.12.19 @ 23:36
-local indent	= 3
-local e24bpc	= true
-local schemes	= { 'shine', 'quiet', 'slate', 'habamax' }
-local scheme_1	= schemes[1]
-local leader	= ';'
+local indent   = 3
+local e24bpc   = true
+local schemes  = { 'shine', 'quiet', 'slate', 'habamax' }
+local scheme_1 = schemes[1]
+local leader   = ';'
 
 --#cmd
 local cmd = vim.cmd
@@ -20,36 +20,36 @@ function set_scheme(k) cmd ('colorscheme '..k) end
 set_scheme (scheme_1)
 
 if os.getenv('TERM_PROGRAM') ~= 'Apple_Terminal' then
-	set ('termguicolors', e24bpc)
+	set ('termguicolors',  e24bpc)
 end
 
-set ('showcmd',			true)
-set ('signcolumn',		'yes')
-set ('splitbelow',		true)
+set ('showcmd',      true)
+set ('signcolumn',  'yes')
+set ('splitbelow',   true)
 
-set ('encoding',			'utf-8')
---set ('syntax',			'enable')
-set ('matchpairs',		{'\':\'', '\":\"', '(:)', '{:}', '[:]', '<:>'})
+set ('encoding',  'utf-8')
+--set ('syntax',  'enable')
+set ('matchpairs',  {'\':\'', '\":\"', '(:)', '{:}', '[:]', '<:>'})
 
-set ('ttyfast',			true)
-set ('updatetime',		300)
-set ('ttimeout',			true)
-set ('ttimeoutlen',		50)
+set ('ttyfast',     true)
+set ('updatetime',   300)
+set ('ttimeout',    true)
+set ('ttimeoutlen',   50)
 
-set ('incsearch',			true)
-set ('hlsearch',			true)
+set ('incsearch',  true)
+set ('hlsearch',   true)
 
-set ('mouse',				'a')
-set ('number',				true)
-set ('rnu',					true)
+set ('mouse',    'a')
+set ('number',  true)
+set ('rnu',     true)
 
-set ('tabstop',			indent)
-set ('softtabstop',		indent)
-set ('shiftwidth',		indent)
-set ('autoindent',		true)
-set ('smartindent',		true)
-set ('expandtab',			false)
-set ('backspace',			{ 'indent', 'eol', 'start' })
+set ('tabstop',      indent)
+set ('softtabstop',  indent)
+set ('shiftwidth',   indent)
+set ('autoindent',     true)
+set ('smartindent',    true)
+set ('expandtab',     false)
+set ('backspace',    { 'indent', 'eol', 'start' })
 
 --#keymap
 function map(mode, map, func, desc)
@@ -57,37 +57,37 @@ function map(mode, map, func, desc)
 end
 vim.g.mapleader = leader
 vim.g.maplocalleader = leader
-map ({'n','v'}, leader, '<nop>')
+map ({'n','v'},  leader,  '<nop>')
 
 --nav
-map ('n',	'<C-k>',		'<C-w><C-k>',					'nav north')
-map ('n',	'<C-j>',		'<C-w><C-j>',					'nav south')
-map ('n',	'<C-l>',		'<C-w><C-l>',					'nav east')
-map ('n',	'<C-h>',		'<C-w><C-h>',					'nav west')
+map ('n',  '<C-k>',  '<C-w><C-k>',  'nav north')
+map ('n',  '<C-j>',  '<C-w><C-j>',  'nav south')
+map ('n',  '<C-l>',  '<C-w><C-l>',   'nav east')
+map ('n',  '<C-h>',  '<C-w><C-h>',   'nav west')
 
 --buffers
-map ('n',	'<S-l>',		':bnext<CR>',					'next buf')
-map ('n',	'<S-h>',		':bprevious<CR>',				'prev buf')
+map ('n',  '<S-l>',  ':bnext<CR>',      'next buf')
+map ('n',  '<S-h>',  ':bprevious<CR>',  'prev buf')
 
 --tabs
-map ('n',	'<C-S-l>',	':tabnext<CR>',				'next tab')
-map ('n',	'<C-S-h>',	':tabprevious<CR>',			'prev tab')
+map ('n',  '<C-S-l>',  ':tabnext<CR>',       'next tab')
+map ('n',  '<C-S-h>',  ':tabprevious<CR>',   'prev tab')
 
 --resize
-map ('n',	'<A-k>',		':resize u2<CR>',				'resize north')
-map ('n',	'<A-j>',		':resize +2<CR>',				'resize south')
-map ('n',	'<A-h>',		':vertical resize -2<CR>',	'resize east')
-map ('n',	'<A-l>',		':vertical resize +2<CR>',	'resize west')
+map ('n',  '<A-k>',  ':resize u2<CR>',           'resize north')
+map ('n',  '<A-j>',  ':resize +2<CR>',           'resize south')
+map ('n',  '<A-h>',  ':vertical resize -2<CR>',   'resize east')
+map ('n',  '<A-l>',  ':vertical resize +2<CR>',   'resize west')
 
 --file
-map ('n',	'<leader>fw',	':w<CR>',					'w')
-map ('n',	'<leader>fa',	':wa<CR>',					'wa')
-map ('n',	'<leader>qq',	':q<CR>',					'q')
-map ('n',	'<leader>qa',	':qa!<CR>',					'qa')
-map ('n',	'<leader>dw',	':close<CR>',				'close')
+map ('n',  '<leader>fw',  ':w<CR>',          'w')
+map ('n',  '<leader>fa',  ':wa<CR>',        'wa')
+map ('n',  '<leader>qq',  ':q<CR>',          'q')
+map ('n',  '<leader>qa',  ':qa!<CR>',       'qa')
+map ('n',  '<leader>dw',  ':close<CR>',  'close')
 
 --misc
-map ('n',	'<leader>h',	':nohl<CR>',				'reset hl')
+map ('n',  '<leader>h',  ':nohl<CR>',  'reset hl')
 
 local last_scheme_index = 1
 function cycle_scheme()
@@ -109,11 +109,11 @@ function set_scheme_index(i)
 	end
 end
 
-map ('n',	'<leader>td',	swap_scheme,				'swap last & default schemes')
-map ('n',	'<leader>tc',	cycle_scheme,				'cycle predefined schemes')
+map ('n',  '<leader>td',  swap_scheme,  'swap last & default schemes')
+map ('n',  '<leader>tc',  cycle_scheme,    'cycle predefined schemes')
 
 for i = 1, 10 do
 	local is = tostring(i)
-	map ('n',	'<leader>t'..is,	set_scheme_index(i),	'scheme index '..is)
+	map ('n',  '<leader>t'..is,  set_scheme_index(i),  'scheme index '..is)
 end
 
